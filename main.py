65aa672d061df8e91680d57574c98c5de217681d
meeramenon07 <46912602+meeramenon07@users.noreply.github.com>
2023-11-28 11:51:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("30 Days Down - What did you think?")
+print()
+for i in range(1, 31):
+  thought = input(f"Day {i}:\n")
+  print()
+  myText = f"You thought Day {i} was"
+  print(f"{myText:^34}")
+  print(f"{thought:^34}")
+  print()
